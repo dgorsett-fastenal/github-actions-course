1d8d30bad13736493087d4f6346633597cf2d207
dgorsett-fastenal <dgorsett@fastenal.com>
2025-10-22 16:20:42 +0000

    don't use ES module syntax

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,49 +1,49 @@
-import { getBooleanInput, getInput, info, setFailed, setSecret } from '@actions/core';
-import { exec, getExecOutput } from '@actions/exec';
+const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-  const baseBranch = getInput('base-branch');
-  const targetBranch = getInput('target-branch');
-  const ghToken = getInput('gh-token');
-  const workingDir = getInput('working-directory');
-  const debug = getBooleanInput('debug');
+  const baseBranch = core.getInput('base-branch');
+  const targetBranch = core.getInput('target-branch');
+  const ghToken = core.getInput('gh-token');
+  const workingDir = core.getInput('working-directory');
+  const debug = core.getBooleanInput('debug');
 
-  setSecret(ghToken);
+  core.setSecret(ghToken);
 
   if (!validateBranchName({ branchName: baseBranch })) {
-    setFailed('Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.');
+    core.setFailed('Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.');
     return;
   }
   
   if (!validateBranchName({ branchName: targetBranch })) {
-    setFailed('Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.');
+    core.setFailed('Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.');
     return;
   }
 
   if (!validateDirectoryName({ dirName: workingDir })) {
-    setFailed('Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes.');
+    core.setFailed('Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes.');
     return;
   }
 
-  info(`[js-dependency-update] : base branch is ${baseBranch}`);
-  info(`[js-dependency-update] : target branch is ${targetBranch}`);
-  info(`[js-dependency-update] : working directory is ${workingDir}`);
+  core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+  core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
+  core.info(`[js-dependency-update] : working directory is ${workingDir}`);
 
-  await exec('npm update', [], {
+  await exec.exec('npm update', [], {
     cwd: workingDir
   });
 
-  const gitStatus = await getExecOutput('git status -s package*.json', [], {
+  const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
     cwd: workingDir
   });
 
   if (gitStatus.stdout.length > 0) {
-    info('[js-dependency-update] : There are updates available!')
+    core.info('[js-dependency-update] : There are updates available!')
   } else {
-    info('[js-dependency-update] : No updates at this point in time.')
+    core.info('[js-dependency-update] : No updates at this point in time.')
   }
 
   /*
@@ -59,7 +59,7 @@ async function run() {
     2. Create a PR to the base-branch using the octokit API
   5. Otherwise, conclude the custom action
   */
-  info('I am a custom JS action');
+  core.info('I am a custom JS action');
 }
  
 run();
